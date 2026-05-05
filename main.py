f7d94b9cc736c84c5efbd35a262747a7ed0d78ec
fujisawaryohei <ryoheifujjiawa@gmail.com>
2026-05-05 12:27:01 +0000

    add checkpointer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import END, StateGraph
 
 from node import answering_node, check_node, selection_node
@@ -27,17 +28,32 @@ def main():
         "check", lambda state: state.current_judge, {True: END, False: "selection"}
     )
 
+    # チェックポインターを設定
+    checkpointer = MemorySaver()
+
     # 作成したグラフのコンパイル
-    compiled = workflow.compile()
+    compiled_graph = workflow.compile(checkpointer=checkpointer)
 
     # ステートの初期化
+    # configuableは独自のdictを定義する事ができるRunnableConfig側で定義された予約キー
+    # LangGraphではチェックポイント記録時に誰がいつ実行したかを識別するためにthread_idを定義してinvoke時に渡す必要がある
+    config = {"configurable": {"thread_id": "exmaple-1"}}
     initial_state = State(query="生成AIについて教えてください")
-    result = compiled.invoke(initial_state)
+    result = compiled_graph.invoke(initial_state, config)
 
     # グラフの画像保存
     with open("graph.png", "wb") as f:
-        f.write(compiled.get_graph().draw_png())
+        f.write(compiled_graph.get_graph().draw_png())
+
+    # メッセージの出力
     print(result["messages"][-1])
+
+    # チェックポイントの出力
+    # for checkpoint in checkpointer.list(config):
+    #     print(checkpoint)
+
+    # 最新のチェックポイントの出力
+    # print_checkpoint_dump(checkpointer, config)
 
 
 if __name__ == "__main__":
---
+++ print_checkpoint.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+
+from langchain_core.runnables import RunnableConfig
+from langgraph.checkpoint.base import BaseCheckpointSaver
+
+
+def print_checkpoint_dump(checkpointer: BaseCheckpointSaver, config: RunnableConfig):
+    checkpoint_tuple = checkpointer.get_tuple(config)
+
+    print("チェックポイントデータ:")
+    pprint(checkpoint_tuple.checkpoint)
+    print("\n メタデータ:")
+    pprint(checkpoint_tuple.metadata)
